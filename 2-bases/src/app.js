6d2e442ab1455630561944b1fdb0db0a28ec92b7
sodi17 <sotodiaz17@gmail.com>
2024-07-21 11:40:56 -0500

    sexto envio

--- 2-bases/src/app.js
+++ 2-bases/src/app.js
@@ -1,6 +1,7 @@
 const {emailTemplate} = require('./js-foundation/01-template');
+console.log(emailTemplate);
 require('./js-foundation/02-destructuring');
 //const templateExports = require('./js-foundation/01-template');
-//console.log(emailTemplate);
 
+ 
 //console.log(templateExports.emailTemplate);
--- 2-bases/src/js-foundation/02-destructuring.js
+++ 2-bases/src/js-foundation/02-destructuring.js
@@ -1,1 +1,11 @@
-console.log(process)
+//console.log(process.env);
+
+const { OneDrive, TMP}=process.env;
+
+//console.table( { OneDrive, TMP});
+
+const characters = ['Naruto','Denji','Rudeus'];
+
+const [Naruto,,]=characters;
+
+//console.log(Naruto);
