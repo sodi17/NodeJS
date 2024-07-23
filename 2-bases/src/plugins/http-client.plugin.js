d0c4eaf9d69d4bad3812d29b91179a3d20fae3af
sodi17 <sotodiaz17@gmail.com>
2024-07-22 20:32:49 -0500

    Nueva version

--- 2-bases/src/plugins/http-client.plugin.js
+++ 2-bases/src/plugins/http-client.plugin.js
@@ -2,10 +2,10 @@ const axios = require('axios');
 const httpClientPlugin={
     get: async (url)=>{
        // const response= await fetch(url);
-       const response= await axios.get(url);
+        const response= await axios.get(url);
       /// const data = await response.json();
-      const data = response.data;
-       return data;
+        const data = response.data;
+        return data;
        // const data = await response.json();
        // return data;
     
